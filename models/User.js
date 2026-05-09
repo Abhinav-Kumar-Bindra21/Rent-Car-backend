2cf3378a9dc0ac88d9dc3b0fe437108bff7f7e67
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-09 22:12:59 +0530

    Created User modal

---
+++ models/User.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      tpye: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["owner", "user"],
+      default: "user",
+    },
+
+    image: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
