b46ed478358a8bfd980c4df7212b5bc513cda4d0
Abhinav kumar Bindra <abhinavbindra1258@gmail.com>
2026-05-11 09:22:51 +0530

    Create Car models and some Owner functioality

--- controllers/ownerController.js
+++ controllers/ownerController.js
@@ -1,10 +1,25 @@
 import User from "../models/User.js";
 
+// API to change role
 export const changeRoleToOwner = async (req, res) => {
   try {
     const { _id } = req.user;
     await User.findByIdAndUpdate(_id, { role: "owner" });
     res.status(200).json({ success: true, message: "Now you can list cars" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Api to list Car
+
+export const addCar = async (req, res) => {
+  try {
+    const { _id } = req.user;
+
+    let car = JSON.parse(req.body.carData);
+
+    const imageFile = req.file;
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
---
+++ models/Cars.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+import { Schema } from "mongoose";
+
+const carSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    brand: {
+      type: String,
+      required: true,
+    },
+
+    model: {
+      type: String,
+      required: true,
+    },
+
+    image: {
+      type: String,
+      required: true,
+    },
+
+    year: {
+      type: number,
+      required: true,
+    },
+
+    category: {
+      type: String,
+      required: true,
+    },
+
+    seating_capacity: {
+      type: Number,
+      required: true,
+    },
+
+    fuel_type: {
+      type: String,
+      required: true,
+    },
+
+    transmission: {
+      type: String,
+      required: true,
+    },
+
+    pricrPerDay: {
+      type: Number,
+      required: true,
+    },
+
+    location: {
+      type: String,
+      required: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+    },
+
+    isAvaliable: {
+      type: String,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+const Car = mongoose.model("Car", carSchema);
+
+export default Car;
